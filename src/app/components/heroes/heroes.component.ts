c9efad1d957f4d0d6c995ed272afa3c79baf66a2
Kyle Baugh <kylebaugh1@gmail.com>
2024-05-06 16:20:33 -0600

    added separate details component and created an injectable/observable service that allows for asynchronous data gathering. Also played with axios to see if I could get it to work, which it does.

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../../interfaces/hero';
 import { HEROES } from '../../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+
+// gathers global state data based on the injectable HeroService class
+import { HeroService } from '../../services/hero.service';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    HeroDetailComponent,
   ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
@@ -17,10 +22,34 @@ import { HEROES } from '../../mock-heroes';
 export class HeroesComponent {
 
   selectedHero?: Hero
-  heroes: Hero[] = HEROES
+  heroes: Hero[] = []
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero
+  }
+
+  // adding the service instance to the constructor gives access to its properties and methods
+  constructor(private heroService: HeroService) {}
+
+  // method on main class that is able to access the Injected Service defined in the constructor
+  getHeroes(): void {
+    // for synchronous code, we can set our data directly
+    this.heroes = this.heroService.getHeroes()
+
+    // for asynchronous code, we need to wait for the promise reponse, and we do that with the subscribe method
+    // this.heroes = this.heroService.getHeroes()
+    //                 .subscribe(heroes => this.heroes = heroes)
+  }
+
+  getMons(): void {
+    this.heroService.getMons()
+  }
+
+  // essentially useEffect in React. As the page initializes, it processes the functions inside.
+  ngOnInit(): void {
+    this.getHeroes()
+    this.getMons()
+
   }
 
 }
---
+++ src/app/services/hero.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from '../interfaces/hero';
+import { HEROES } from '../mock-heroes';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  // creates funtion that returns an array of objects matching the Hero interface
+  // Would most likely use this to work with an API for actual data
+   getHeroes(): Hero[]{
+     return HEROES
+   }
+
+  // with an observable, we do this
+  // getHeroes will return an Observable array of Hero objects
+  // getHeroes(): Observable<Hero[]>{
+  //   const heroes = of(HEROES)
+  //   return heroes
+  // }
+
+  getMons(){
+    axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then(res => {
+        console.log(res.data)
+        return res.data
+      })
+      .catch(err => {
+        console.log(err)
+        return err.data
+      })
+
+  }
+
+  constructor() { }
+}
